13bd4964a41ffd3229bf4127ed49678f1217eb9b
wojtek <wojtek.kostanski@gmail.com>
2018-11-19 23:05:31 +0100

    show weather by filling form

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,2 +1,16 @@
 class WeathersController < ApplicationController
+  def home; end
+
+  def create
+    validator = CityNameValidator::Schema.call(city: params[:city])
+    if validator.success?
+      session[:weather] = GetWeatherService.new(params[:city]).call
+      redirect_to weather_path(params[:city])
+    else
+      render :home
+    end
+  end
+  def show
+    @city_weather = session[:weather]
+  end
 end
