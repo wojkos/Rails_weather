b4bdbc6edb119a28eec49102e481f4084857de70
wojtek <wojtek.kostanski@gmail.com>
2018-11-20 23:06:37 +0100

    use service to create new city

---
+++ app/services/create_city_service.rb
@@ -0,0 +1,15 @@
+class CreateCityService
+  def initialize(params)
+    @city = params
+  end
+
+  def call
+    if City.find_by(name: @city).nil?
+      City.create!(name: @city, views: 1)
+    else
+      city = City.find_by(name: @city)
+      city.views += 1
+      city.save!
+    end
+  end
+end
--- app/services/get_weather_service.rb
+++ app/services/get_weather_service.rb
@@ -6,11 +6,28 @@ class GetWeatherService
   end
 
   def call
+    response = get_response(@city)
+
+    if response['code'] == '404'
+      { 'cod' => '404', 'message' => 'city not found' }
+    else
+      add_city(@city)
+      response
+    end
+  end
+
+  private
+
+  def get_response(city)
     url =
-      "https://api.openweathermap.org/data/2.5/weather?q=#{@city}"\
+      "https://api.openweathermap.org/data/2.5/weather?q=#{city}"\
       "&APPID=#{ENV.fetch('OPENWEATHER_MAP_API_KEY')}"
     url = URI.encode(url)
     resp = Net::HTTP.get_response(URI.parse(url))
     JSON.parse(resp.body)
+  end
+
+  def add_city(city)
+    CreateCityService.new(city).call
   end
 end
