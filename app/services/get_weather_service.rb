a87bbf4804c5abb7c0bb3b610c8fe91e0b613a5a
wojtek <wojtek.kostanski@gmail.com>
2018-11-20 21:35:33 +0100

    fix encoding problem

--- app/services/get_weather_service.rb
+++ app/services/get_weather_service.rb
@@ -6,9 +6,11 @@ class GetWeatherService
   end
 
   def call
-    url = "https://api.openweathermap.org/data/2.5/weather?q=#{@city}&APPID="+ENV.fetch('OPENWEATHER_MAP_API_KEY')
+    url =
+      "https://api.openweathermap.org/data/2.5/weather?q=#{@city}"\
+      "&APPID=#{ENV.fetch('OPENWEATHER_MAP_API_KEY')}"
+    url = URI.encode(url)
     resp = Net::HTTP.get_response(URI.parse(url))
-    data = JSON.parse(resp.body)
-    p data
+    JSON.parse(resp.body)
   end
 end
