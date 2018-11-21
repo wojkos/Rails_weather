ba6a23536636060b7d780b0c8003d014cbbec1d3
Wojtek Kostanski <wojtek@tappest.com>
2018-11-21 10:16:42 +0100

    add seeds

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+## Create databese and add come seed's
+`rake db:setup`
 ## Add env variables
 Add `.env`  file to main app folder. In file add  OPENWEATHER_MAP_API_KEY variable.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+City.create!(name: 'warsaw', views: 1)
+City.create!(name: 'paris', views: 1)
+City.create!(name: 'london', views: 1)
+City.create!(name: 'berlin', views: 1)
+City.create!(name: 'tokyo', views: 1)
